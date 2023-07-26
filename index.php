36b3928e6f2ba5d8069bd64aaa871f12d4551e49
Patricio <contacto.patricio.01@gmail.com>
2023-07-25 20:58:41 -0600

    subi los primeros archivos

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos-index.css">
+    <title>Castillo relojes</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <nav>
+            <ul>
+                <a href="">
+                    <li>Catalogo</li>
+                </a>
+                <a href="">
+                    <li>Categorias</li>
+                </a>
+                <a href="">
+                    <li>Nosotros</li>
+                </a>
+            </ul>
+        </nav>
+        <img src="" alt="">
+        <h1><span class="castillo">Castillo</span> relojes</h1>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+        <p id="sugerencias">Mas vendidos</p>
+        <hr>
+
+        <div class="articulo">
+            <img src="" alt="">
+            <h3 class="nombre">uno</h3>
+            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, voluptate.</p>
+        </div>
+        <div class="articulo">
+            <img src="" alt="">
+            <h3 class="nombre">dos</h3>
+            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quisquam?</p>
+        </div>
+        <div class="articulo">
+            <img src="" alt="">
+            <h3 class="nombre">tres</h3>
+            <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque?</p>
+        </div>
+        <div class="ver-catalogo"><a href="">Ver catálogo</a></div>
+
+        <section>
+            <p id="sugerencias">Sugerencias</p>
+            <hr>
+            <div class="sugerido">
+                <div class="articulo">
+                    <img src="" alt="">
+                    <h3 class="nombre">uno</h3>
+                    <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque?
+                    </p>
+                </div>
+            </div>
+            <div class="sugerido">
+                <div class="articulo">
+                    <img src="" alt="">
+                    <h3 class="nombre">dos</h3>
+                    <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque?
+                    </p>
+                </div>
+            </div>
+            <div class="sugerido">
+                <div class="articulo">
+                    <img src="" alt="">
+                    <h3 class="nombre">tres</h3>
+                    <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque?
+                    </p>
+                </div>
+            </div>
+            <div class="sugerido">
+                <div class="articulo">
+                    <img src="" alt="">
+                    <h3 class="nombre">cuatro</h3>
+                    <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque?
+                    </p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <!-- FOOTER -->
+    <footer>
+        <table>
+            <td>
+                <tr>
+                    <a href="">
+                        <p>Fromas de pago</p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p>Preguntas frecuentes</p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p></p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p></p>
+                    </a>
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    <a href="">
+                        <p></p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p>Contacto</p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p>Acerca de</p>
+                    </a>
+                </tr>
+                <tr>
+                    <a href="">
+                        <p>Terminos y condiciones</p>
+                    </a>
+                </tr>
+            </td>
+ 
+            <p class="derechos"> Derechos reservados 2023</p>
+        </table>
+    </footer>
+</body>
+
+</html>
